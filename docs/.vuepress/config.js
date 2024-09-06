49eca24a29ae1c57e954779e3971abe6d3803bc9
Rory <rory@ferrory.net>
2024-09-06 13:54:08 +0200

    Fer rory patch 7 (#9)
    * Create readme.md
    
    * Add files via upload
    
    * Update config.js
    
    * Add files via upload
    
    * Delete docs/.vuepress/public/images/readme.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ export default defineUserConfig({
   description: 'Op deze website kun je de boekencollectie van (Fer)Rory vinden. De boekencollectie is verdeeld in drie categorieën: Devops, Persoonlijke Financiën en Zelf verbetering.',
 
   theme: defaultTheme({
-    
+    logo: '/images/logo.png',
     navbar: ['/', '/boeken.md', 'contact.md'],
   }),
 
